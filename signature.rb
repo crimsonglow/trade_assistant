afabf4386397c69f858c742f3509ee6f857ecb80
crimsonglovv <crimsonglovv.working@gmai.com>
2022-02-17 13:30:27 +0200

    version 1.0

--- signature.rb
+++ signature.rb
@@ -1,14 +1,10 @@
 module Binance
   class SignatureBuilder
     class << self
-      def signature_bild(payload:)
-        digest = OpenSSL::Digest::SHA256.new
-        OpenSSL::HMAC.hexdigest(digest, API_KEY || SECRET_KEY, payload)
-      end
-
-      def signature(parameters:)
+      def request(parameters:)
         payload = parameters.map { |key, value| "#{key}=#{value}" }.join("&")
-        signature_bild(payload: payload)
+        digest = OpenSSL::Digest::SHA256.new
+        OpenSSL::HMAC.hexdigest(digest, SECRET_KEY, payload)
       end
     end
   end
